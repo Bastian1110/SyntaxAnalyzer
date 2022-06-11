f80dffbbad5852d8d0edbe6f74c3731de22e840e
Sebastian Mora <chebimora02@gmail.com>
2022-06-11 18:31:33 -0500

    Paralelism Implementation

--- main.py
+++ main.py
@@ -3,20 +3,51 @@
 from SyntaxAnalizer import syntaxAnalize
 
 import os
+import sys
+import time
+import multiprocessing
 from distutils.dir_util import copy_tree
+
+def chunker(lista, chunks):
+    return list((lista[i::chunks] for i in range(chunks)))
+
+def secuencial(archivos):
+  for name in archivos:
+    syntaxAnalize(name)
+    os.remove(name)
+
+
+def paralela(archivos,cores):
+  pool = multiprocessing.Pool()
+  pool = multiprocessing.Pool(processes=cores)
+  inputs = chunker(archivos,cores)
+  pool.map(secuencial,inputs)
+  
+
+
 
 
 if __name__ == '__main__':
-  print("Copying the original Input folder ...")
+  print(" * Actividad Integradora Final * ")
+  print("Creando la carpeta de Output ...")
   copy_tree('./Input','./Output')
-  path =r'./Output'
-  list_of_files = []
+  print("Listo !!!")
 
+  path =r'./Output'
+  archivos = []
   for root, dirs, files in os.walk(path):
     for file in files:
-      list_of_files.append(os.path.join(root,file))
-  print("Analyzing all files ...")
-  for name in list_of_files:
-    syntaxAnalize(name)
-    os.remove(name)
-  print("Read!")
+      if file.endswith('.txt'):
+        archivos.append(os.path.join(root,file))
+
+  print("- Prueba Secuencial ----------")
+  start = time.time()
+  secuencial(archivos)
+  end = time.time()
+  print("Tiempo aproximado de ejecucion secuencial: ", end-start)
+
+  print("- Prueba Paralela -----------")
+  start = time.time()
+  paralela(archivos,2)
+  end = time.time()
+  print("Tiempo aproximado de ejecucion paralela: ", end-start)
